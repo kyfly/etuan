2053450a3eb14e45c459c202560641727f5640c7
LiC <1078054388@qq.com>
2014-07-28 19:48:43 +0800

    admin应用原生滚动条，去除nicescroll.js。查看报名结果的样式基本完成。更换侧边栏配色（初步）。

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,21 +1,5 @@
 
 (function() {
-    "use strict";
-
-    // custom scrollbar
-
-    $("html").niceScroll({styler:"fb",cursorcolor:"#65cea7", cursorwidth: '6', cursorborderradius: '0px', background: '#424f63', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
-
-    $(".left-side").niceScroll({styler:"fb",cursorcolor:"#65cea7", cursorwidth: '3', cursorborderradius: '0px', background: '#424f63', spacebarenabled:false, cursorborder: '0'});
-
-
-    $(".left-side").getNiceScroll();
-    if ($('body').hasClass('left-side-collapsed')) {
-        $(".left-side").getNiceScroll().hide();
-    }
-
-
-
     // Toggle Left Menu
    jQuery('.menu-list > a').click(function() {
       
@@ -66,3 +50,6 @@
    });
 
 })(jQuery);
+	 $(document).ready(function () {
+        $('#main').css('min-height', $(window).outerHeight(true) - $('#nav').outerHeight(true) - $('#footer').outerHeight(true) + "px");
+    });
