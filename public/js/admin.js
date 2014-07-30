114fb386b05fad16d31c7ee092deb9e728d50e74
LiC <1078054388@qq.com>
2014-07-30 19:47:01 +0800

    报名表编辑样式基本完成。

--- public/js/admin.js
+++ public/js/admin.js
@@ -50,6 +50,208 @@
    });
 
 })(jQuery);
+
 	 $(document).ready(function () {
         $('#main').css('min-height', $(window).outerHeight(true) - $('#nav').outerHeight(true) - $('#footer').outerHeight(true) + "px");
-    });
+		$('#sidebar').load('../sidebar.html');
+		});
+		
+		originalheight();
+		$("#extraform").children(".form-group").hide();
+		//添加元素到左边
+		$("#studentID2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#studentID1").show();
+			});
+		$("#studentname2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#studentname1").show();
+			});
+		$("#sex2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#sex1").show();
+			});
+		$("#xueyuan2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#xueyuan1").show();
+			});
+		$("#major2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#major1").show();
+			});
+		$("#specialty2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#specialty1").show();
+			});
+		$("#Email2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#Email1").show();
+			});
+		$("#QQ2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#QQ1").show();
+			});
+		$("#phonenumlong2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#phonenumlong1").show();
+			});
+		$("#phonenumshort2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#phonenumshort1").show();
+			});
+		$("#origin2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#origin1").show();
+			});
+		$("#wish1-2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#wish1").show();
+			});
+		$("#wish2-2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#wish2").show();
+			});
+		$("#wish3-2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#wish3").show();
+			});
+		$("#tiaoji2").click(function(){
+			changeheight();
+			$(this).hide();
+			$("#tiaoji1").show();
+			});
+
+		function originalheight() {	
+			var height1 , height2 , maxheight;
+			height1 = $('#extraform').outerHeight(true);
+			height2 = $('#extralist').outerHeight(true);
+			if ( height1 > height2 ){ maxheight = height1; }
+				else { maxheight = height2; }
+			$('#addform').css('height', maxheight - 300 + "px");
+			};
+		function changeheight() {	
+			var height1 , height2 , maxheight;
+			height1 = $('#extraform').outerHeight(true);
+			height2 = $('#extralist').outerHeight(true);
+			if ( height1 > height2 ){ maxheight = height1; }
+				      		   else { maxheight = height2; }
+			$('#addform').css('height', maxheight + 200 + "px");
+			};
+		//删除按钮
+		$("#delete1").click(function(){
+			changeheight();
+			$("#studentID1").hide();
+			$("#studentID2").show();
+			});
+		$("#delete2").click(function(){
+			changeheight();
+			$("#studentname1").hide();
+			$("#studentname2").show();
+			});
+		$("#delete3").click(function(){
+			changeheight();
+			$("#sex1").hide();
+			$("#sex2").show();
+			});
+		$("#delete4").click(function(){
+			changeheight();
+			$("#xueyuan1").hide();
+			$("#xueyuan2").show();
+			});
+		$("#delete5").click(function(){
+			changeheight();
+			$("#major1").hide();
+			$("#major2").show();
+			});
+		$("#delete6").click(function(){
+			changeheight();
+			$("#specialty1").hide();
+			$("#specialty2").show();
+			});
+		$("#delete7").click(function(){
+			changeheight();
+			$("#Email1").hide();
+			$("#Email2").show();
+			});
+		$("#delete8").click(function(){
+			changeheight();
+			$("#QQ1").hide();
+			$("#QQ2").show();
+			});
+		$("#delete9").click(function(){
+			changeheight();
+			$("#phonenumlong1").hide();
+			$("#phonenumlong2").show();
+			});
+		$("#delete10").click(function(){
+			changeheight();
+			$("#phonenumshort1").hide();
+			$("#phonenumshort2").show();
+			});
+		$("#delete11").click(function(){
+			changeheight();
+			$("#origin1").hide();
+			$("#origin2").show();
+			});
+		$("#delete12").click(function(){
+			changeheight();
+			$("#wish1").hide();
+			$("#wish1-2").show();
+			});
+		$("#delete13").click(function(){
+			changeheight();
+			$("#wish2").hide();
+			$("#wish2-2").show();
+			});
+		$("#delete14").click(function(){
+			changeheight();
+			$("#wish3").hide();
+			$("#wish3-2").show();
+			});
+		$("#delete15").click(function(){
+			changeheight();
+			$("#tiaoji1").hide();
+			$("#tiaoji2").show();
+			});
+		//向上移动
+		$(".moveup").click(function(){
+			var content;
+			content = $(this).parents(".form-group");
+  			content.insertBefore(content.prev());
+			});
+		//向下移动
+		$(".movedown").click(function(){
+			var content2;
+			content2 = $(this).parents(".form-group");
+  			content2.insertAfter(content2.next());
+			});
+
+	
+		$('.form_datetime').datetimepicker({
+			language:  'en',
+			format: 'yyyy/mm/dd hh:ii',
+			weekStart: 1,
+			todayBtn:  1,
+			autoclose: 1,
+			todayHighlight: 1,
+			startView: 2,
+			forceParse: 0,
+			showMeridian: 1
+		});
+		
+		$(function () { $("[data-toggle='tooltip']").tooltip(); });
+		 
