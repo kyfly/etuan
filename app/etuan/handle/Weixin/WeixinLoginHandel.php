09b61872fd1567df8a32189268c1b41085512e72
liuJD <erchuochuo@163.com>
2014-09-02 12:38:21 +0800

    添加判断输入学号是否正确

--- app/etuan/handle/Weixin/WeixinLoginHandel.php
+++ app/etuan/handle/Weixin/WeixinLoginHandel.php
@@ -4,11 +4,11 @@ class Weixin
 	public static function login($user){
 		$name = WxUser::where("wx_uid",$user)->pluck("nick_name");
 		if(!$name){
-			return false;
+			return 'false';
 		}
 		Session::put("nick_name", $name);
 		Session::put("wx_uid", $user);
-		return true;
+		return 'true';
 	}
 	public static function logout(){
 		Session::forget('nick_name');
