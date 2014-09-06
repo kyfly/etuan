0906951eb4fb6c4b71de8afcc98ae849451611cf
yzf <youzhenfei@126.com>
2014-09-06 17:27:54 +0800

    修改报名表和社团介绍入口页面的js，成功载入所有信息

--- public/js/loadAllOrg.js
+++ public/js/loadAllOrg.js
@@ -27,9 +27,7 @@ $(document).ready(function () {
         $(this).css('border', '1px solid #ddd');
     });
 
-    //TODO：记得修改地址！！！
-    //$.get('/registration/organization-registration', function (data, status) {
-    $.get('org.json', function (data, status) {
+    $.get('/organization/organization-info', function (data, status) {
         if (status == 'success') {
             var regDivTpl = ' <div class="col-xs-12 col-sm-4 col-md-3">' +
                 '<a href="{0}">' +
@@ -46,14 +44,14 @@ $(document).ready(function () {
             var school = [];
             for (var i = 0; i < data.length; i++) {
                 var logoUrl = data[i].logo_url.split('.');
-                var orgUrl = 'http://www.etuan.org/shetuan/' + data[i].org_id;
+                var orgUrl = '/shetuan/' + data[i].org_id;
                 data[i].logo_url += '@300w.' + logoUrl[logoUrl.length - 1];
                 var regDiv = String.format(regDivTpl,
-                    orgUrl, data[i].org_name, data[i].logo_url);
-                if (data[i].org_type == '校级组织') {
+                    orgUrl, data[i].name, data[i].logo_url);
+                if (data[i].type == '校级组织') {
                     $('#universityLevel').append(regDiv);
                 }
-                else if (data[i].org_type == '院级组织') {
+                else if (data[i].type == '院级组织') {
                     var schoolIndex = school.indexOf(data[i].school);
                     if (schoolIndex < 0) {
                         schoolIndex = school.length;
