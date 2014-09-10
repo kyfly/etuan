49c96a60cb84632b883542721ed23941c17672c3
liuJD <erchuochuo@163.com>
2014-09-10 11:00:08 +0800

    抽奖限制为新生

--- app/controllers/choujiangController.php
+++ app/controllers/choujiangController.php
@@ -9,6 +9,13 @@ class choujiangController extends BaseController
 	}
 	//获取某用户参与抽奖活动结果
 	public function get(){
+		$wx_uid = Weixin::user();
+		$stu_id = WxUser::where('wx_uid',$wx_uid)->pluck('stu_id');
+        if(substr($stu_id, 0,2) != '14'){
+            $msgArr = array('title' => '错误', 'body' => '真遗憾,这次抽奖活动是为新生准备的,你不能参与',
+            'status' => 'error', 'btn' => 'true','action'=>'close');
+            return View::make('showmessage')->with('messageArr', $msgArr);
+        }
 		$result = $this->lot->getInfo();
 		return urldecode(json_encode($result));
 	}
