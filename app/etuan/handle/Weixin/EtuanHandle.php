a72f411cb46d34e04df19fa65fdae0a8e5627817
liuJD <erchuochuo@163.com>
2014-09-11 21:55:47 +0800

    添加转发多客服功能，带调试

--- app/etuan/handle/Weixin/EtuanHandle.php
+++ app/etuan/handle/Weixin/EtuanHandle.php
@@ -41,6 +41,28 @@ class EtuanHandle extends replyHandle
         return $this->reply($postObj,$content);
 
     }
+    public function check_what_send($postObj){
+        if($postObj->ToUserName == 'gh_255607335c38'){  //判断该微信号是否为团团一家服务号，不是则返回
+            if($postObj->MsgType == 'text')     //判断消息类型是否为文本的，是文本则查看这条消息内容是否存在于关键字列表，如果是则直接返回信息，否则转多客服
+            {
+                $mp_id = Wxdata::where('mp_origin_id',$postObj->ToUserName)->pluck('mp_id');
+                $keywords = Keyword::where('mp_id',$mp_id)->list('keyword');
+                $i = 0;
+                while ($keywords[$i]) {
+                    if($keywords[$i] == $postObj->Content)
+                        return $this->reply($postObj,$content);
+                    $i++;
+                }
+                return WB::sendSeviceMsg($postObj);
+            }elseif($postObj->MsgType == 'event'){  //为事件消息不做处理，直接返回
+                return true;
+            }else{  //为其他消息类型，转多客服
+                return WB::sendSeviceMsg($postObj);
+            }
+        }else{
+            return true;
+        }
+    }
     private function sceneReply($postObj,$scene_id){
          $mp_origin_id = $postObj->ToUserName;
          $result = Etuan::where("scene_id",$scene_id)->select("act_type","act_id")->first();
@@ -48,7 +70,6 @@ class EtuanHandle extends replyHandle
          $obj =new $activity;
          $key = $obj->primaryKey;
          $url = $activity::where($key,$result->act_id)->select("name", "org_uid")->first();
-         //$org_uid = Wxdata::where('mp_origin_id',$mp_origin_id)->pluck('org_uid');
          $actObj = new actNewHandle;
          $acturl = $actObj->getactUrl($activity,$result->act_id);
          $arr =["title"=>$url->name,"description"=>"点击进入".$url->name.">>",
