c2691a20b57f0bcd04dc2bdd8dbcbd58fff7c471
Wrq <ruqiangaileying@gmail.com>
2014-09-28 23:04:08 +0800

    organizationController文件编码不是utf-8导致excel

--- app/controllers/organization/organizationController.php
+++ app/controllers/organization/organizationController.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 class organizationController extends BaseController
 {
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'development' => array('hui-PC','helloworld-PC','liujiandong','LenovoV480','masf','matp','PC-20140426NFBB'),
+	'development' => array('Wrq-pc','hui-PC','helloworld-PC','liujiandong','LenovoV480','masf','matp','PC-20140426NFBB'),
 
 ));
 
