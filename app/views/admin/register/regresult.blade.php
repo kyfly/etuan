0144b5b98ec10504ca6f5a97a4259654b137e35e
yzf <youzhenfei@126.com>
2014-10-18 16:35:54 +0800

    支持多张报名结果的显示

--- app/views/admin/register/regresult.blade.php
+++ app/views/admin/register/regresult.blade.php
@@ -13,12 +13,28 @@
 	<div class="tab-content">
         <!--查看结果-->
         <div class="tab-pane active in" id="regresult">
-          <ul class="nav nav-tabs" role="tablist">
-<!--            <li class="active"><a href="#tongji" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-list"></span>&nbsp;统计报表</a></li>-->
-<!--            <li><a href="#dantiao" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-th-list"></span>&nbsp;单张详情</a></li>-->
-            <li class="active"><a href="#biaoge" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;总体详情</a></li>
-            <li><a href="#xiazai" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-download"></span>&nbsp;下载结果</a></li>
-            </ul>
+                <div class="row">
+                <div class="col-md-7">
+                    <ul class="nav nav-tabs" role="tablist">
+        <!--            <li class="active"><a href="#tongji" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-list"></span>&nbsp;统计报表</a></li>-->
+        <!--            <li><a href="#dantiao" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-th-list"></span>&nbsp;单张详情</a></li>-->
+                    <li class="active"><a href="#biaoge" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;总体详情</a></li>
+                    <li><a href="#xiazai" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-download"></span>&nbsp;下载结果</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-5" style="padding-top: 5px">
+                    <form class="form-horizontal" role="form">
+                      <div class="form-group">
+                        <label for="regSelect" class="col-sm-4 control-label">选择报名表</label>
+                        <div class="col-sm-8">
+                          <select id="regSelect" style="width: 180px">
+
+                          </select>
+                        </div>
+                      </div>
+                    </form>
+                </div>
+            </div>
           <!-- Tab panes -->
           <div class="tab-content">
             <div class="tab-pane active" id="biaoge" style="padding-top: 10px">
@@ -50,7 +66,7 @@
                     </div>
                     <div class="col-md-5">
                         <p><br>导出为PDF文档，可以打印为单张报名表，适用于面试等场合。</p>
-                        <a href="/registration/downloadpdf" type="button" class="btn btn-info btn-lg btn-block">下载pdf文件</a>
+                        <a id="downPdf" href="/registration/downloadpdf" type="button" class="btn btn-info btn-lg btn-block">下载pdf文件</a>
                     </div>
                 </div>
                 <div class="col-md-12" style="margin-top: 30px">
@@ -59,7 +75,7 @@
                     </div>
                     <div class="col-md-5">
                         <p><br>导出为Excel电子表格，在一张表格中查看全部报名结果，适用于数据分析、总体统计。</p>
-                        <a href="/registration/downloadxls" type="button" class="btn btn-info btn-lg btn-block">下载xls文件</a>
+                        <a id="downXls" href="/registration/downloadxls" type="button" class="btn btn-info btn-lg btn-block">下载xls文件</a>
                     </div>
                 </div>
                <!--<button type="button" class="btn btn-info btn-lg btn-block">导出为pdf格式</button>-->
@@ -75,5 +91,38 @@
 <script src="http://cdn.kyfly.net/lib/js/jquery.min.js"></script>
 <script src="http://cdn.kyfly.net/lib/js/bootstrap.min.js"></script>
 <script src="/js/admin.js"></script>
+<script>
+    (function ($) {
+        $.getUrlParam = function (name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(reg);
+            if (r != null) return decodeURIComponent(r[2]);
+            return null;
+        }
+    })(jQuery);
+
+    $(document).ready(function() {
+        $.get('/registration/activitylist', function(data, status){
+            if (status == 'success'){
+                data = eval(data);
+                for (var i = 0; i < data.length; i++)
+                {
+                    var option = '<option value="' + data[i].reg_id + '">' + data[i].name + '</option>';
+                    $('#regSelect').append(option);
+                }
+                var currentId = $.getUrlParam('activityId');
+                if (currentId)
+                {
+                    $('#regSelect').val(currentId);
+                    $('#downPdf').attr('href', '/registration/downloadpdf?activityId=' + currentId);
+                    $('#downXls').attr('href', '/registration/downloadxls?activityId=' + currentId);
+                }
+            }
+        });
+        $('#regSelect').change(function() {
+           window.location.href = '/admin/register/regresult?activityId=' + $(this).val();
+        });
+    })
+</script>
 </body>
 </html>
