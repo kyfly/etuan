5d84662ed280b820eb188e26f4b6b9f2e2d4e481
evilucifero <evilucifero@163.com>
2014-12-09 22:49:15 +0800

    修改部分语法==变为===，更改innerHTML写法为.text()，增加选择时的校验

--- public/js/touparticipator.js
+++ public/js/touparticipator.js
@@ -1,5 +1,20 @@
-// JavaScript Document
-
+function scrollTop() {
+	var posNow = $(document).scrollTop();
+	var scrollStep = 300;
+	if (posNow > 0) {
+		window.scroll(0, posNow - scrollStep);
+		scrollTopTimer = setTimeout("scrollTop()", 10);
+	}
+	else {
+		clearTimeout(scrollTopTimer);
+	}
+};
+function toggleScrollTopImg() {
+	if ($(document).scrollTop() > 0)
+		$('#scrollUp').show();
+	else
+		$('#scrollUp').hide();
+};
 $(document).ready(function () {
 
 	$(window).scroll(toggleScrollTopImg);
@@ -11,40 +26,49 @@ $(document).ready(function () {
 	var flag = new Array();
 	var _checked_item = 0;
 
-	for(var i = 0; i < _total_item;i++){
+	for(var i = 0; i < _total_item; i++){
 		flag[i] = 0;
 	}
 
-	if(_type == "pic"){
+	if(_type === "pic"){
 		$(".choosebtn").click(function () {
+			if(_checked_item > _limit_choice) {
+            	alert("当前所选已经超过" + _limit_choice + "个");
+            	return;
+        	}
 			var idval = $(this).attr("id");
 			var num = idval.substr(6,2);
-			if (flag[num-1] == 0) {
-				flag[num-1] = 1;
+			if (flag[num - 1] === 0) {
+				flag[num - 1] = 1;
 				$(this).parent(".thumbnail").css('border', '1px solid #a8d154');
 				$(this).prev(".chosen").css('display', 'block');
-				var x = document.getElementById(idval);
-				x.innerHTML = "点击取消";
+				$(idval).text("点击取消");
 				_checked_item ++;
 				$('#current_choice').text(_checked_item);
-			} else {
-				flag[num-1] = 0;
+			}
+			else {
+				flag[num - 1] = 0;
 				$(this).parent(".thumbnail").css('border', '1px solid #ddd');
 				$(this).prev(".chosen").css('display', 'none');
-				var x = document.getElementById(idval);
-				x.innerHTML = "点击选定";
+				$(idval).text("点击选定");
 				_checked_item --;
 				$('#current_choice').text(_checked_item);
 			}
 		});
-	}else if (_type == "text"){
+	}
+	else if (_type === "text"){
 		$("input.checkbox").click(function(){
+			if(_checked_item > _limit_choice) {
+            	alert("当前所选已经超过" + _limit_choice + "个");
+            	return;
+        	}
 			var num2 = $(this).attr("value");
-			if (flag[num2-1] == 0) {
-				flag[num2-1] = 1;
+			if (flag[num2 - 1] === 0) {
+				flag[num2 - 1] = 1;
 				_checked_item ++;
-			}else {
-				flag[num2-1] = 0;
+			}
+			else {
+				flag[num2 - 1] = 0;
 				_checked_item --;
 			}
 			$('#current_choice').text(_checked_item);
@@ -53,21 +77,21 @@ $(document).ready(function () {
 	
 	$("#submit").click(function()
     {   
-        if(_checked_item == 0){
+        if(_checked_item === 0){
             alert("你还没有做出自己的选择呢");
-        }else if(_checked_item > _limit_choice){
+        }
+        else if(_checked_item > _limit_choice){
             alert("当前所选已经超过" + _limit_choice + "个");
-        }else{
+        }
+        else{
             var participatorInfo = {
                 choices:[]
             };
-			
 			for(var i = 0; i < _total_item;i++){
 				if(flag[i] == 1){
 					participatorInfo.choices.push(i+1);
 				}
-			}
-			
+			}	
             var sendJson = {
                 activityId:_activityId,
                 participatorInfo:JSON.stringify(participatorInfo)
@@ -126,22 +150,3 @@ $(document).ready(function () {
     });
 	
 });
-
-function toggleScrollTopImg() {
-	if ($(document).scrollTop() > 0)
-		$('#scrollUp').show();
-	else
-		$('#scrollUp').hide();
-};
-
-function scrollTop() {
-	var posNow = $(document).scrollTop();
-	var scrollStep = 300;
-	if (posNow > 0) {
-		window.scroll(0, posNow - scrollStep);
-		scrollTopTimer = setTimeout("scrollTop()", 10);
-	}
-	else {
-		clearTimeout(scrollTopTimer);
-	}
-};
